06d7b9a4a105348e01e216c5fa3794d0ae01d622
jungjaechoi <joong8812@gmail.com>
2022-01-20 09:56:43 +0900

    change - The method to check if same user is already registered

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model  # 사용자가 데이터베이스 안에 있는 검사하는 함수
 
 
 # Create your views here.
@@ -13,20 +14,22 @@ def sign_up_view(request):
         password2 = request.POST.get('password2', None)
         bio = request.POST.get('bio', None)
 
-        # 기존에 있는 유저인지 확인
-        user = UserModel.objects.get(username=username)
-        if user is not None:
-            return render(request, 'user/signup.html')
-
         if password != password2:
             return render(request, 'user/signup.html')
         else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
-        
+            # 기존에 있는 유저인지 확인
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+
+            UserModel.objects.create_user(
+                username=username, password=password, bio=bio)
+            # new_user = UserModel()
+            # new_user.username = username
+            # new_user.password = password
+            # new_user.bio = bio
+            # new_user.save()
+
         return redirect('/sign-in')
 
 
@@ -34,11 +37,11 @@ def sign_in_view(request):
     if request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
-        
+
         me = UserModel.objects.get(username=username)
         if me.password == password:
             request.session['user'] = me.username
-            return HttpResponse(me.username) # 로그인 후 유저네임을 응답한다
+            return HttpResponse(me.username)  # 로그인 후 유저네임을 응답한다
         else:
             return redirect('/sign-in')
     elif request.method == 'GET':
