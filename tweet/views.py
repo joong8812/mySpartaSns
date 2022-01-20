c1d1468f9a4c8a771599526b051811c874fb1aff
jungjaechoi <joong8812@gmail.com>
2022-01-20 17:16:15 +0900

    add - detail_tweet, write_comment, delete_comment

--- tweet/views.py
+++ tweet/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import TweetModel
+from .models import TweetModel, TweetComment
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -26,6 +26,45 @@ def tweet(request):
         my_tweet.content = request.POST.get('my-content', '')
         my_tweet.save()
         return redirect('/tweet')
+
+
+def detail_tweet(request, id):
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            this_tweet = TweetModel.objects.get(id=id)
+            all_comments = TweetComment.objects.filter(tweet_id=id).order_by('-created_at')
+            return render(request, 'tweet/tweet_detail.html', {'tweet': this_tweet, 'comment': all_comments})
+        else:
+            return redirect('/sign-in')
+
+
+def write_comment(request, id):
+    if request.method == 'POST':
+        user = request.user.is_authenticated
+        if user:
+            tweet = TweetModel.objects.get(id=id)
+            my_comment = TweetComment()
+            my_comment.author = request.user
+            my_comment.tweet = tweet
+            my_comment.comment = request.POST.get('comment', '')
+            my_comment.save()
+            return redirect(f'/tweet/{id}')
+        else:
+            return redirect('/sign-in')
+
+
+@login_required
+def delete_comment(request, id):
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            my_comment = TweetComment.objects.get(id=id)
+            tweet_id = my_comment.tweet.id
+            my_comment.delete()
+            return redirect(f'/tweet/{tweet_id}')
+        else:
+            return redirect('/sign-in')
 
 
 @login_required
