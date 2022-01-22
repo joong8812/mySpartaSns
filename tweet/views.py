46bb21540746df4ed4237f10c3b64711efc90d8d
jungjaechoi <joong8812@gmail.com>
2022-01-22 20:56:31 +0900

    add - tag feature

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView, TemplateView
 from django.shortcuts import render, redirect
 from .models import TweetModel, TweetComment
 from django.contrib.auth.decorators import login_required
@@ -22,12 +23,17 @@ def tweet(request):
     elif request.method == 'POST':
         user = request.user
         content = request.POST.get('my-content', '')
+        tags = request.POST.get('tag', '').split(',')
 
         if content == '':
             all_tweet = TweetModel.objects.all().order_by('-created_at')
             return render(request, 'tweet/home.html', {'error': '글은 공백일 수 없습니다.', 'tweet':all_tweet})
         else:
             my_tweet = TweetModel.objects.create(author=user, content=content)
+            for tag in tags:
+                tag = tag.strip()
+                if tag != '':
+                    my_tweet.tags.add(tag)
             my_tweet.save()
             return redirect('/tweet')
 
@@ -76,3 +82,20 @@ def delete_tweet(request, id):
     my_tweet = TweetModel.objects.get(id=id)
     my_tweet.delete()
     return redirect('/tweet')
+
+
+class TagCloudTV(TemplateView):
+    template_name = 'taggit/tag_cloud_view.html'
+
+
+class TaggedObjectLV(ListView):
+    template_name = 'taggit/tag_with_post.html'
+    model = TweetModel
+
+    def get_queryset(self):
+        return TweetModel.objects.filter(tags__name=self.kwargs.get('tag'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tagname'] = self.kwargs['tag']
+        return context
