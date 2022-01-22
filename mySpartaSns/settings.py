19de22dd5ad5c77e2b342a9b4ff66efd9cdbbf6a
jungjaechoi <joong8812@gmail.com>
2022-01-22 21:03:42 +0900

    change - ready to push to Gtihub

--- mySpartaSns/settings.py
+++ mySpartaSns/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n$lwtr5=tdu_t-4gwgz5&0phmd+s(%cbhhl+_@du&ze$m&nwx)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
